d7500e9c061f7f9d8a218182003de88c4b57c68c
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 11:51:13 +0500

    Откатвывает изменения в master

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
 <script>
-  let numbers = [1, 2, 3, 4];
-
-  const addNumber = () => {
-    numbers = [...numbers, numbers.length + 1];
-  };
-
-  $: sum = numbers.reduce((t, n) => t + n, 0);
+  export let name;
 </script>
 
-<p>{numbers.join(' + ')} = {sum}</p>
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
 
-<button on:click={addNumber}>
-  Добавить ещё одно число
-</button>
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
