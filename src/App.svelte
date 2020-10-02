05fe7cc97ab0b4c11eff05169df59ad450f02af5
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 12:25:04 +0500

    Добавляет код для 04. Блоки else-if

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,11 @@
 <script>
-  export let name;
+  let x = 7;
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+{#if x > 10 }
+  <p>{x} больше чем 10</p>
+{:else if 5 > x}
+  <p>{x} меньше 5</p>
+{:else}
+  <p>{x} между 5 и 10</p>
+{/if}
