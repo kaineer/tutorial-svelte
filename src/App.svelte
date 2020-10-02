719ef21e9564ee890050e25285cbec9233c8ba11
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 12:29:18 +0500

    Добавляет код для 04. Блоки each

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,17 @@
 <script>
-  export let name;
+  let cats = [
+    { id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+    { id: 'z_AbfPXTKms', name: 'Maru' },
+    { id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+  ];
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Известные коты на YouTube</h1>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<ul>
+  {#each cats as cat, i}
+    <li>{i + 1}: <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+        {cat.name}
+      </a></li>
+  {/each}
+</ul>
