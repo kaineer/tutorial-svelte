2bc621521ddb9e6a7bcf7b4e068bde3cefda8a60
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 10:31:22 +0500

    Добавляет код со стилем
    и что-то там про барсука

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
-<script>
-  export let name;
-</script>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  p {
+    color: purple;
+    font-family: 'Comic Sans MS', cursive;
+    font-size: 2em;
   }
 </style>
+
+<p>
+Тихо в лесу,<br>
+сопит, неслышно, барсук<br>
+он устал.
+</p>
