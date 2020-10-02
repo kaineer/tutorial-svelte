0c49171d5edf7fae7dda99712ac2b5367b828051
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 12:11:55 +0500

    Добавляет код для 03. Пропсы в объекте

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,12 @@
 <script>
-  export let name;
+  import Info from './Info.svelte';
+
+  const pkg = {
+    name: 'svelte',
+    version: 3,
+    speed: 'blazing',
+    website: 'https://svelte.dev'
+  };
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Info {...pkg} />
