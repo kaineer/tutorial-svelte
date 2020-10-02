eac44c92d6031b9cd56581577147f19b2865b27f
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-02 11:16:51 +0500

    Добавляет код для задания 02. Обновление объектов

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-  export let name;
+  let numbers = [1, 2, 3, 4];
+
+  const addNumber = () => {
+    numbers = [...numbers, numbers.length + 1];
+  };
+
+  $: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<p>{numbers.join(' + ')} = {sum}</p>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<button on:click={addNumber}>
+  Добавить ещё одно число
+</button>
