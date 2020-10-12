137488c69d8c81116f8ee5dc15327031e6d456b4
Sergey Kluchkovsky <kaineer@gmail.com>
2020-10-12 16:24:09 +0500

    Копирует начальное состояние

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,16 @@
 <script>
-  export let name;
+  let a = 1;
+  let b = 2;
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<label>
+  <input type=number value={a} min=0 max=10>
+  <input type=range value={a} min=0 max=10>
+</label>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<label>
+  <input type=number value={b} min=0 max=10>
+  <input type=range value={b} min=0 max=10>
+</label>
+
+<p>{a} + {b} = {a + b}</p>
